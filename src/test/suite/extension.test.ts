fc0b120122e41389edc763853986c2136583682b
荣顶 <wangrongding@qq.com>
2022-02-10 16:55:05 +0800

    style: 修改代码风格样式

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -9,7 +9,7 @@ suite("Extension Test Suite", () => {
   vscode.window.showInformationMessage("Start all tests.");
 
   test("Sample test", () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+    assert.strictEqual(-1, [1, 2, 3,].indexOf(5));
+    assert.strictEqual(-1, [1, 2, 3,].indexOf(0));
   });
 });
